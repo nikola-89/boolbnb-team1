eb16bd1a2e6d1d18d803056d90fd04a41e3a7350
Nicola <nicola.ba1989@gmail.com>
2020-04-14 20:16:35 +0200

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(ApartmentsTableSeeder::class);
         $this->call(ServicesTableSeeder::class);
+        $this->call(ApartmentsTableSeeder::class);
     }
 }
